e81a250402891b934abe3d464abbc4e07b6219f6
Eddy Hernandez <eddy.josafat@basekit.com>
2015-08-26 14:32:36 +0100

    p68

--- ch02/gear.js
+++ ch02/gear.js
@@ -1,6 +1,10 @@
-function Gear (chainring, cog) {
+'use strict';
+
+function Gear (chainring, cog, rim, tire) {
     this.chainring = chainring;
     this.cog = cog;
+    this.rim = rim;
+    this.tire = tire;
 }
 
 Gear.prototype = {
@@ -21,10 +25,34 @@ Gear.prototype = {
         this._cog = value;
     },
 
+    get rim () {
+        return this._rim;
+    },
+
+    set rim (value) {
+        this._rim = value;
+    },
+
+    get tire () {
+        return this._tire;
+    },
+
+    set tire (value) {
+        this._tire = value;
+    },
+
     get ratio () {
         return this.chainring / this.cog;
+    },
+
+    get gearInches () {
+        return this.ratio * (this.rim + (this.tire * 2));
     }
 }
 
-console.log((new Gear(52, 11)).ratio);
-console.log((new Gear(30, 27)).ratio);
+console.log((new Gear(52, 11, 26, 1.5)).gearInches);
+console.log((new Gear(52, 11, 24, 1.25)).gearInches);
+
+// We can have instances initialised in other parts of the app using just 2 arguments as it used to be
+// and then try to use in the new area
+console.log((new Gear(52, 11)).gearInches); // NaN
