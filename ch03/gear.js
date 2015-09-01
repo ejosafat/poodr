13d8f06ced8ea5d0db1a742c4f035e48f526786b
Eddy Hernandez <eddy.josafat@basekit.com>
2015-09-01 12:43:20 +0100

    p126

---
+++ ch03/gear.js
@@ -0,0 +1,85 @@
+'use strict';
+
+function Gear (chainring, cog, rim, tire) {
+    this.chainring = chainring;
+    this.cog = cog;
+    this.rim = rim;
+    this.tire = tire;
+}
+
+Gear.prototype = {
+    constructor: Gear,
+    get chainring () {
+        return this._chainring;
+    },
+
+    set chainring (value) {
+        this._chainring = value;
+    },
+
+    get cog () {
+        return this._cog;
+    },
+
+    set cog (value) {
+        this._cog = value;
+    },
+
+    get rim () {
+        return this._rim;
+    },
+
+    set rim (value) {
+        this._rim = value;
+    },
+
+    get tire () {
+        return this._tire;
+    },
+
+    set tire (value) {
+        this._tire = value;
+    },
+
+    get ratio () {
+        return this.chainring / this.cog;
+    },
+
+    get gearInches () {
+        return this.ratio * (new Wheel(this.rim, this.tire).diameter);
+    }
+}
+
+function Wheel (rim, tire) {
+    this.rim = rim;
+    this.tire = tire;
+}
+
+Wheel.prototype = {
+    constructor: Wheel,
+    get rim () {
+        return this._rim;
+    },
+
+    set rim (value) {
+        this._rim = value;
+    },
+
+    get tire () {
+        return this._tire;
+    },
+
+    set tire (value) {
+        this._tire = value;
+    },
+
+    get diameter () {
+        return this.rim + (this.tire * 2);
+    },
+
+    get circumference () {
+        return this.diameter * Math.PI;
+    }
+};
+
+console.log((new Gear(52, 11, 26, 1.5)).gearInches);
