49bfd83c7b5da52b9bdba2eaa02eff1a34ec9f60
Eddy Hernandez <eddy.josafat@basekit.com>
2015-09-18 22:42:17 +0100

    Superclass 1st iteration

--- ch06/bicycle.js
+++ ch06/bicycle.js
@@ -1,31 +1,41 @@
 'use strict';
 
 function Bicycle (args) {
-    this._size = args.size;
-    this._tapeColor = args.tapeColor;
 }
 
 Bicycle.prototype = {
     constructor: Bicycle,
+}
 
-    get size () {
+function RoadBike (args) {
+    this._size = args.size;
+    this._tapeColor = args.tapeColor;
+}
+
+RoadBike.prototype = Object.create(Bicycle.prototype);
+RoadBike.prototype.constructor = RoadBike;
+
+Object.defineProperty(RoadBike.prototype, 'size', {
+    get: function () {
         return this._size;
-    },
+    }
+});
 
-    get tapeColor () {
+Object.defineProperty(RoadBike.prototype, 'tapeColor', {
+    get: function () {
         return this._tapeColor;
-    },
+    }
+});
 
-    get spares () {
+Object.defineProperty(RoadBike.prototype, 'spares', {
+    get: function () {
         return {
             chain: '10-speed',
             tireSize: '23',
             tapeColor: this.tapeColor
         };
     }
-
-    //...
-}
+});
 
 function MountainBike (args) {
     this._frontShock = args.frontShock;
@@ -35,30 +45,37 @@ function MountainBike (args) {
 
 MountainBike.prototype = Object.create(Bicycle.prototype);
 MountainBike.prototype.constructor = MountainBike;
+
 Object.defineProperty(MountainBike.prototype, 'frontShock', {
     get: function () {
         return this._frontShock;
     }
 });
+
 Object.defineProperty(MountainBike.prototype, 'rearShock', {
     get: function () {
         return this._rearShock;
     }
 });
+
 Object.defineProperty(MountainBike.prototype, 'spares', {
     get: function () {
-        var parts = Bicycle.prototype.spares;
+        var parts = Bicycle.prototype.spares || {};
         parts.rearShock = this.rearShock;
         parts.frontShock = this.frontShock;
         return parts;
     }
 });
 
+var roadBike = new RoadBike({
+    size: 'M',
+    tapeColor: 'red'
+});
+console.log(roadBike.size);
+
 var mountainBike = new MountainBike({
     size: 'S',
     frontShock: 'Manitou',
     rearShock: 'Fox'
 });
-
 console.log(mountainBike.size);
-console.log(mountainBike.spares);
