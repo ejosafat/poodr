7c1d3d8f7ba0f39a94fdccf4fc447564b00054d8
Eddy Hernandez <eddy.josafat@basekit.com>
2015-11-13 19:28:19 +0000

    Using a diameterizable

--- ch09/gear.test.js
+++ ch09/gear.test.js
@@ -2,16 +2,18 @@ describe('Gear', function () {
     'use strict';
 
     var Gear = require('./sources/gear');
-    var Wheel = require('./sources/wheel');
     var assert = require('chai').assert;
+    var diameterizable = {
+        diameter: 10
+    };
 
     it('calculates gear inches', function () {
         var gear = new Gear({
             chainring: 52,
             cog: 11,
-            wheel: new Wheel(26, 1.5)
+            wheel: diameterizable
         });
 
-        assert.closeTo(137.1, gear.gearInches, 0.01);
+        assert.closeTo(47.27, gear.gearInches, 0.01);
     });
 });
